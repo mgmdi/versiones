6da58e866ae2a6918e54d0ed2e26d257ecfeaa48
Javier <jav393@gmail.com>
2019-12-02 19:42:56 -0400

    Se implemento tomar el tiempo de internet para el timestamp

--- src/main/python/common.py
+++ src/main/python/common.py
@@ -1,5 +1,8 @@
 import netifaces as ni
 import Pyro4
+import datetime
+import time
+from urllib.request import urlopen
 
 def get_ip_address():
     # Find interface with assigned ip address
@@ -33,3 +36,14 @@ def run_server(server, ip, server_port, server_no):
         except: # TODO: AVERIGUAR CUAL ES LA EXCEPCION PARA ABORTAR EN LAS OTRAS
             server_port += 1
             server_no += 1
+
+
+def get_utc_time():
+    res = urlopen('http://just-the-time.appspot.com/')
+    result = res.read().strip()
+    result_str = result.decode('utf-8')
+    x = result_str.split(" ")
+    x1 = x[0].split("-")
+    date = x1[1] + "/" + x1[2] + "/" + x1[0] + " " + x[1]
+    timestamp = time.mktime(datetime.datetime.strptime(date, '%m/%d/%Y %H:%M:%S').timetuple())
+    return timestamp
