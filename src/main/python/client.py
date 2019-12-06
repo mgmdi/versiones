669d67ae1a816aadb5a7022b0cdcc039c890d94e
mgmdi <14-10444@usb.ve>
2019-12-06 08:55:01 -0400

    minor fix

--- src/main/python/client.py
+++ src/main/python/client.py
@@ -17,6 +17,7 @@ class mywindow(QtWidgets.QMainWindow):
     client_ip = get_ip_address()
 
     #servers = find_servers()
+    servers = "servers"
 
     array = ["File 1","File 2","File 3","File 4","File 5"]
 
@@ -41,12 +42,12 @@ class mywindow(QtWidgets.QMainWindow):
 
     def btnUpdate(self):
         self.ui.label_2.setText("Se hizo update del archivo: " + self.ui.comboBox.currentText())
-        update(self.ui.comboBox.currentText(),self.client_ip,servers)
+        update(self.ui.comboBox.currentText(),self.client_ip,self.servers)
 
 
     def btnCheckout(self):
         self.ui.label_2.setText("Se hizo checkout del archivo: " + self.ui.comboBox.currentText())
-        checkout(self.ui.comboBox.currentText(),self.client_ip,self.ui.comboBox2.currentText(),servers)
+        checkout(self.ui.comboBox.currentText(),self.client_ip,self.ui.comboBox_2.currentText(),self.servers)
 
     def btnCommit(self):
         self.ui.label_5.setText("Commit del archivo: " + self.ui.lineEdit.text())
@@ -55,8 +56,13 @@ class mywindow(QtWidgets.QMainWindow):
     def selectionChange(self):
         self.ui.label_2.setText("Selecciono la opcion: " + self.ui.comboBox.currentText())
         self.ui.comboBox_2.clear()
-        self.ui.comboBox_2.addItems(self.array2[int(self.ui.comboBox.currentText())-1])
-
+        #self.ui.comboBox_2.addItems(self.array2[int(self.ui.comboBox.currentText())-1])
+        versiones = servers.getVersions(self.ui.comboBox.currentText(),self.client_ip)
+        versiones = versiones.keys()
+        versiones_arreglo = []
+        for v in versiones:
+            versiones_arreglo.append(v)
+        self.ui.comboBox_2.addItems(versiones_arreglo)
  
 def find_servers():
 
@@ -70,10 +76,12 @@ def find_servers():
     return server
 
 def update(file_name, ip,servers):
-    servers.update(file_name, ip)
+    #servers.update(file_name, ip)
+    print("Update")
 
 def checkout(file_name,ip,date,servers):
-    servers.checkout(file_name, ip, date)
+    #servers.checkout(file_name, ip, date)
+    print("Checkout")
 
 def commit(file_name,ip):
     versiones_dir = op.abspath(op.join(__file__, op.pardir, op.pardir, op.pardir, op.pardir))
