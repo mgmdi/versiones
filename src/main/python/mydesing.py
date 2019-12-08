95bd80c2d20bfcd2d6fe101578fe547aa39022a5
Javier <jav393@gmail.com>
2019-12-08 14:00:37 -0400

    Se agrego la funcion del nombre de usuario

--- src/main/python/mydesing.py
+++ src/main/python/mydesing.py
@@ -2,18 +2,16 @@
 
 # Form implementation generated from reading ui file 'ejemplo.ui'
 #
-# Created by: PyQt5 UI code generator 5.13.2
+# Created by: PyQt5 UI code generator 5.10.1
 #
 # WARNING! All changes made in this file will be lost!
 
-
 from PyQt5 import QtCore, QtGui, QtWidgets
-
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(446, 364)
+        MainWindow.resize(466, 364)
         MainWindow.setMinimumSize(QtCore.QSize(373, 338))
         MainWindow.setMaximumSize(QtCore.QSize(466, 364))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
@@ -25,46 +23,52 @@ class Ui_MainWindow(object):
         self.pushButton.setGeometry(QtCore.QRect(10, 370, 91, 33))
         self.pushButton.setObjectName("pushButton")
         self.comboBox = QtWidgets.QComboBox(self.centralwidget)
-        self.comboBox.setGeometry(QtCore.QRect(20, 40, 161, 25))
+        self.comboBox.setGeometry(QtCore.QRect(30, 90, 161, 25))
         self.comboBox.setObjectName("comboBox")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(0, 130, 381, 17))
+        self.label_2.setGeometry(QtCore.QRect(10, 130, 381, 17))
         self.label_2.setText("")
         self.label_2.setObjectName("label_2")
         self.comboBox_2 = QtWidgets.QComboBox(self.centralwidget)
-        self.comboBox_2.setGeometry(QtCore.QRect(210, 40, 191, 25))
+        self.comboBox_2.setGeometry(QtCore.QRect(200, 90, 191, 25))
         self.comboBox_2.setObjectName("comboBox_2")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(90, 20, 54, 17))
+        self.label.setGeometry(QtCore.QRect(90, 70, 54, 17))
         self.label.setObjectName("label")
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(270, 20, 111, 17))
+        self.label_3.setGeometry(QtCore.QRect(250, 70, 111, 17))
         self.label_3.setObjectName("label_3")
         self.label_4 = QtWidgets.QLabel(self.centralwidget)
-        self.label_4.setGeometry(QtCore.QRect(10, 170, 101, 31))
+        self.label_4.setGeometry(QtCore.QRect(10, 200, 101, 31))
         self.label_4.setObjectName("label_4")
         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(40, 210, 221, 33))
+        self.lineEdit.setGeometry(QtCore.QRect(40, 240, 221, 33))
         self.lineEdit.setObjectName("lineEdit")
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(10, 260, 91, 33))
+        self.pushButton_2.setGeometry(QtCore.QRect(280, 240, 91, 33))
         self.pushButton_2.setObjectName("pushButton_2")
         self.label_5 = QtWidgets.QLabel(self.centralwidget)
-        self.label_5.setGeometry(QtCore.QRect(110, 260, 231, 31))
+        self.label_5.setGeometry(QtCore.QRect(10, 290, 231, 31))
         self.label_5.setText("")
         self.label_5.setObjectName("label_5")
         self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(50, 80, 91, 33))
+        self.pushButton_3.setGeometry(QtCore.QRect(60, 160, 91, 33))
         self.pushButton_3.setObjectName("pushButton_3")
         self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_4.setGeometry(QtCore.QRect(260, 80, 91, 33))
+        self.pushButton_4.setGeometry(QtCore.QRect(250, 160, 91, 33))
         self.pushButton_4.setObjectName("pushButton_4")
         self.label_6 = QtWidgets.QLabel(self.centralwidget)
-        self.label_6.setGeometry(QtCore.QRect(10, 210, 101, 31))
+        self.label_6.setGeometry(QtCore.QRect(10, 240, 101, 31))
         self.label_6.setObjectName("label_6")
+        self.label_7 = QtWidgets.QLabel(self.centralwidget)
+        self.label_7.setGeometry(QtCore.QRect(10, 40, 121, 17))
+        self.label_7.setObjectName("label_7")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(130, 30, 191, 33))
+        self.lineEdit_2.setObjectName("lineEdit_2")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 446, 22))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 466, 23))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -86,3 +90,5 @@ class Ui_MainWindow(object):
         self.pushButton_3.setText(_translate("MainWindow", "Update"))
         self.pushButton_4.setText(_translate("MainWindow", "Checkout"))
         self.label_6.setText(_translate("MainWindow", "File:"))
+        self.label_7.setText(_translate("MainWindow", "Nombre de usuario:"))
+
